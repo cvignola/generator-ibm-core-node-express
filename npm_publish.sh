00156e8b53e706576b6b649474dbda77438a0d81
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 01:13:27 -0500

    set npm publish branch to master

--- npm_publish.sh
+++ npm_publish.sh
@@ -3,16 +3,16 @@ set -ev
 
 npm test
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     git config user.email "travis@travis-ci.org"
     git config user.name "Travis CI"
     git checkout -b increment-patch-version
-    npm version patch -m "[ci skip] Increment version to %s"
+    npm version patch -m "[ci skip] Increment package version to %s"
     npm publish
-    git branch --set-upstream-to origin/development
-    git push origin HEAD:development
-    git push origin HEAD:development --tags
+    git branch --set-upstream-to origin/master
+    git push origin HEAD:master
+    git push origin HEAD:master --tags
 else
-    echo "Not publishing to NPM. isPullRequest?: ${TRAVIS_PULL_REQUEST}, branch: ${TRAVIS_BRANCH}"
+    echo "Not publishing to NPM because build is a pull request (${TRAVIS_PULL_REQUEST}) or branch not master (${TRAVIS_BRANCH})"
 fi
