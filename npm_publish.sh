49425cde3c798e08ddebeca0bdac095b9e989d25
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 10:50:57 -0500

    do not set user config to Travis, build emails need to be sent to commiter

--- npm_publish.sh
+++ npm_publish.sh
@@ -6,11 +6,9 @@ npm test
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     git checkout -b increment-patch-version
-    npm version patch -m "[ci skip] Increment package version to %s"
+    npm version patch -m "[ci skip] Travis CI - Increment package version to %s"
     npm publish
     git branch --set-upstream-to origin/master
-    git config user.email "travis@travis.ibm.com"
-    git config user.name "Travis IBM CI"
     git push origin HEAD:master
     git push origin HEAD:master --tags
 else
