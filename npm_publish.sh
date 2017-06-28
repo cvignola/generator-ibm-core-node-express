c685f7e7063735473310360fa4ab68eb69a7dc20
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 10:51:44 -0500

    set to development to test email notification

--- npm_publish.sh
+++ npm_publish.sh
@@ -3,14 +3,14 @@ set -ev
 
 npm test
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     git checkout -b increment-patch-version
     npm version patch -m "[ci skip] Travis CI - Increment package version to %s"
     npm publish
-    git branch --set-upstream-to origin/master
-    git push origin HEAD:master
-    git push origin HEAD:master --tags
+    git branch --set-upstream-to origin/development
+    git push origin HEAD:development
+    git push origin HEAD:development --tags
 else
-    echo "Not publishing to NPM because build is a pull request (${TRAVIS_PULL_REQUEST}) or branch not master (${TRAVIS_BRANCH})"
+    echo "Not publishing to NPM because build is a pull request (${TRAVIS_PULL_REQUEST}) or branch not development (${TRAVIS_BRANCH})"
 fi
