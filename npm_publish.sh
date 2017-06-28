d678bd8bf0fc1015527d6d70e36db5c9e9ef3a7c
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 10:17:19 -0500

    set user to travis after npm publish, so build failure emails gets sent to correct emails

--- npm_publish.sh
+++ npm_publish.sh
@@ -5,12 +5,12 @@ npm test
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
-    git config user.email "travis@travis.ibm.com"
-    git config user.name "Travis IBM CI"
     git checkout -b increment-patch-version
     npm version patch -m "[ci skip] Increment package version to %s"
     npm publish
     git branch --set-upstream-to origin/master
+    git config user.email "travis@travis.ibm.com"
+    git config user.name "Travis IBM CI"
     git push origin HEAD:master
     git push origin HEAD:master --tags
 else
