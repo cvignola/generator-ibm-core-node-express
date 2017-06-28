9296a7f8c01fdd7062acff85aa30d2846b4633f0
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 00:33:13 -0500

    set upstream to correct branch

--- npm_publish.sh
+++ npm_publish.sh
@@ -9,8 +9,9 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development
     git config user.name "Travis CI"
     npm version patch -m "[ci skip] Increment version to %s"
     npm publish
-    git remote add origin https://${GH_TOKEN}@github.ibm.com/arf/generator-core-node-express.git
-    git push origin development --tags
+    # git remote add origin https://${GH_TOKEN}@github.ibm.com/arf/generator-core-node-express.git
+    git branch --set-upstream-to=origin/development
+    git push --tags
 else
     echo "Not publishing to NPM. isPullRequest?: ${TRAVIS_PULL_REQUEST}, branch: ${TRAVIS_BRANCH}"
 fi
