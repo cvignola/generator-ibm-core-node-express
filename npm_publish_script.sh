7c0f0ac586494589cf9606304dac3ed766464ff1
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 14:37:04 -0500

    set git user email before build

--- npm_publish_script.sh
+++ npm_publish_script.sh
@@ -4,14 +4,6 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     
-    echo "commit: ${TRAVIS_COMMIT}"
-    USER_EMAIL=$(git --no-pager show -s --format='%ae' "${TRAVIS_COMMIT}")
-    USER_NAME=$(git --no-pager show -s --format='%an' "${TRAVIS_COMMIT}")
-    echo "user email: ${USER_EMAIL}"
-    echo "user name: ${USER_NAME}"
-    git config user.email "${USER_EMAIL}"
-    git config user.name "${USER_NAME}"
-    
     git checkout -b increment-patch-version
     npm version patch -m "[ci skip] Travis CI - Increment package version to %s"
     npm publish
