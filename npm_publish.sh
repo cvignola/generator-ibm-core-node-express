d2d82757b571c64d80abb9be5eb8230f090e7c96
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 00:23:57 -0500

    add github origin

--- npm_publish.sh
+++ npm_publish.sh
@@ -7,9 +7,10 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     git config user.email "travis@travis-ci.org"
     git config user.name "Travis CI"
-    npm version patch -m "Increment version to %s"
+    npm version patch -m "[ci skip] Increment version to %s"
     npm publish
-    git push --tags
+    git remote add origin https://${GH_TOKEN}@github.ibm.com/arf/generator-core-node-express.git
+    git push origin development --tags
 else
     echo "Not publishing to NPM. isPullRequest?: ${TRAVIS_PULL_REQUEST}, branch: ${TRAVIS_BRANCH}"
 fi
