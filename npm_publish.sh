0fd353b103f4b6dac42abce0536aea47882e8657
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 01:08:11 -0500

    set correct branches to checkout and push

--- npm_publish.sh
+++ npm_publish.sh
@@ -7,9 +7,12 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     git config user.email "travis@travis-ci.org"
     git config user.name "Travis CI"
+    git checkout -b increment-patch-version
     npm version patch -m "[ci skip] Increment version to %s"
     npm publish
-    git push && git push --tags
+    git branch --set-upstream-to origin/development
+    git push origin HEAD:development
+    git push origin HEAD:development --tags
 else
     echo "Not publishing to NPM. isPullRequest?: ${TRAVIS_PULL_REQUEST}, branch: ${TRAVIS_BRANCH}"
 fi
