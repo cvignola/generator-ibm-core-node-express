faa4ed60d829e25fe5a948569edf371818f9efb9
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 13:25:59 -0500

    change script name and add logging for script

--- build_publish_script.sh
+++ build_publish_script.sh
@@ -6,6 +6,7 @@ npm test
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     
+    echo "commit: ${TRAVIS_COMMIT}"
     USER_EMAIL=$(git --no-pager show -s --format='%ae' "${TRAVIS_COMMIT}")
     USER_NAME=$(git --no-pager show -s --format='%an' "${TRAVIS_COMMIT}")
     echo "user email: ${USER_EMAIL}"
