a1225508355e0a7bae392df61ce1ed414e13e06f
Danny D. Cao <ddcao@us.ibm.com>
2017-06-27 22:10:08 -0500

    add npm auth token

--- npm_publish.sh
+++ npm_publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" > .npmrc
+echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
 git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
 npm version patch -m "Increment version to %s"
 npm publish
-git push
+git push --tags
