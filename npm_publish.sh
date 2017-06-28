3a9b481341fd1775aa73243b4db15d813eaabf7c
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 13:13:56 -0500

    set git user name and email, needed to run npm version patch and git push

--- npm_publish.sh
+++ npm_publish.sh
@@ -5,9 +5,18 @@ npm test
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
+    
+    USER_EMAIL=$(git --no-pager show -s --format='%ae' "${TRAVIS_COMMIT}")
+    USER_NAME=$(git --no-pager show -s --format='%an' "${TRAVIS_COMMIT}")
+    echo "user email: ${USER_EMAIL}"
+    echo "user name: ${USER_NAME}"
+    git config user.email "${USER_EMAIL}"
+    git config user.name "${USER_NAME}"
+    
     git checkout -b increment-patch-version
     npm version patch -m "[ci skip] Travis CI - Increment package version to %s"
     npm publish
+    
     git branch --set-upstream-to origin/development
     git push origin HEAD:development
     git push origin HEAD:development --tags
