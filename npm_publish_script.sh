4dd7b80a46d85c870458af5b6710022cc07d64ce
Danny D. Cao <ddcao@us.ibm.com>
2017-06-28 14:26:53 -0500

    cleanup

--- npm_publish_script.sh
+++ npm_publish_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "//npm-registry.whitewater.ibm.com/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
     
     echo "commit: ${TRAVIS_COMMIT}"
@@ -16,9 +16,9 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "development
     npm version patch -m "[ci skip] Travis CI - Increment package version to %s"
     npm publish
     
-    git branch --set-upstream-to origin/development
-    git push origin HEAD:development
-    git push origin HEAD:development --tags
+    git branch --set-upstream-to origin/master
+    git push origin HEAD:master
+    git push origin HEAD:master --tags
 else
-    echo "Not publishing to NPM because build is a pull request (${TRAVIS_PULL_REQUEST}) or branch not development (${TRAVIS_BRANCH})"
+    echo "Not publishing to NPM because build is a pull request (${TRAVIS_PULL_REQUEST}) or branch not master (${TRAVIS_BRANCH})"
 fi
